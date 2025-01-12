4ee6b9cc51e61170b66502b4b8156146f1b085d7
Agnieszka Roguska <agnieszka.roguska@yahoo.com>
2025-01-12 19:40:00 +0100

    added db connection

--- src/main/java/pl/edu/vistula/firstrestapispring/product/domain/Product.java
+++ src/main/java/pl/edu/vistula/firstrestapispring/product/domain/Product.java
@@ -1,8 +1,17 @@
 package pl.edu.vistula.firstrestapispring.product.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue
     private Long id;
     private String name;
+
+    public Product(){}
 
     public Product(String name){
         this.name = name;
