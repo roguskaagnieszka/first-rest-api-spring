3b1d8767c4704445b3260bc5084eaea3c70733c6
Agnieszka Roguska <agnieszka.roguska@yahoo.com>
2025-01-12 19:45:33 +0100

    modified exception handling

--- src/main/java/pl/edu/vistula/firstrestapispring/product/support/ecxeption/ProductNotFoundException.java
+++ src/main/java/pl/edu/vistula/firstrestapispring/product/support/ecxeption/ProductNotFoundException.java
@@ -3,6 +3,5 @@ package pl.edu.vistula.firstrestapispring.product.support.ecxeption;
 public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException(Long id) {
         super("Product with id " + id + " not found");
-//        super(String.format("Product with id %d not found", id));
     }
 }
